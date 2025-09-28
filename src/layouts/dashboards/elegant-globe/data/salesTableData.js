7b9791c8a49eaa2936f72c6bbe815ca17169f903
Santelot <santi@gnius.club>
2025-09-27 18:53:04 -0600

    Create salesTableData.js

---
+++ src/layouts/dashboards/elegant-globe/data/salesTableData.js
@@ -0,0 +1,44 @@
+// main/src/layouts/dashboards/elegant-globe/data/salesTableData.js
+
+// Images
+import US from "assets/images/icons/flags/US.png";
+import DE from "assets/images/icons/flags/DE.png";
+import GB from "assets/images/icons/flags/GB.png";
+import BR from "assets/images/icons/flags/BR.png";
+import AU from "assets/images/icons/flags/AU.png";
+
+
+const salesTableData = [
+  {
+    country: [US, "Estados Unidos"],
+    sales: 2500,
+    value: "$230,900",
+    bounce: "29.9%",
+  },
+  {
+    country: [DE, "Alemania"],
+    sales: 3900,
+    value: "$440,000",
+    bounce: "40.22%",
+  },
+  {
+    country: [GB, "Reino Unido"],
+    sales: 1400,
+    value: "$190,700",
+    bounce: "23.44%",
+  },
+  {
+    country: [BR, "Brasil"],
+    sales: 562,
+    value: "$143,960",
+    bounce: "32.14%",
+  },
+  {
+    country: [AU, "Australia"],
+    sales: 400,
+    value: "$100,000",
+    bounce: "56.83%",
+  },
+];
+
+export default salesTableData;
